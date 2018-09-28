bef2d71b8f43dd0a742ce7b86a63a9a32281c338
Damián Cabello <jdamiancabello@gmail.com>
2018-09-28 09:58:54 +0200

    Ahora tiene un archivo de txt con el TODO de la APP.

--- app/src/main/java/com/example/usuario/sendmessage/ViewMessageActivity.java
+++ app/src/main/java/com/example/usuario/sendmessage/ViewMessageActivity.java
@@ -5,10 +5,26 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
+/**
+ * Esta clase recibe un mensaje de otra actividad. La etiqueta de Javadoc @link
+ * se utiliza en la definición de clase o metodo. No despues de las etiquetas @author
+ * ni @Version. En ese caso se debe usar @see
+ *
+ * {@link SendMessage_MainActivity}
+ *
+ * @author Damián cabello
+ * @version 1.0
+ * @see SendMessage_MainActivity
+ *
+ * @see <a target="_blank" href="http://google.es">Ir a Google.es</a>
+ */
+
 public class ViewMessageActivity extends AppCompatActivity {
 
     private EditText msg;
     private EditText autor;
+
+    //TODO tener cuidado que esta clase recibe un mensaje importante
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
