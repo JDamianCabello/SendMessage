a523f92511a5c92477bc551cb248239c0a1b546e
Damián Cabello <jdamiancabello@gmail.com>
2018-10-01 12:01:39 +0200

    Ya está funcional v2.

--- app/src/main/java/com/example/usuario/sendmessage/ViewMessageActivity.java
+++ app/src/main/java/com/example/usuario/sendmessage/ViewMessageActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.usuario.sendmessage.model.Message;
